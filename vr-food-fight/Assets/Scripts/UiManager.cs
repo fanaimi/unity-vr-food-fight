de7c1a8862f3f053266fa33c8dee7ffc0becaf91
fanaimi <fanaimi@users.noreply.github.com>
2022-01-08 19:14:58 +0000

    added UIManager and setting up script

--- vr-food-fight/Assets/Scripts/UiManager.cs
+++ vr-food-fight/Assets/Scripts/UiManager.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UiManager : MonoBehaviour
 {
+    [SerializeField] private TMP_Text secsLeftTxt;
+    [SerializeField] private TMP_Text numOfTargetsTxt;
+
+    private int secsLeft = 0;
+    private int numOfTargets = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +22,16 @@ public class UiManager : MonoBehaviour
     {
         
     }
+
+    public void SetTimer()
+    {
+        
+    } // SetTimer
+
+    public void UpdateUI()
+    {
+        
+    } // UpdateUI
+    
+
 }
