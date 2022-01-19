729d001cab93e24b1534a8019def6f26fb3baf1c
fanaimi <fanaimi@users.noreply.github.com>
2022-01-19 21:15:08 +0000

    adding bullet and adding rotation to ammo

--- vr-food-fight/Assets/Interactions/Scripts/Gun.cs
+++ vr-food-fight/Assets/Interactions/Scripts/Gun.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Gun : GrabbableObject
 {
+
+    [SerializeField] private Bullet m_bulletPrefab;
+    [SerializeField] private Transform m_munition;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +21,9 @@ public class Gun : GrabbableObject
     {
         base.OnInteractionStart();
         print("gun here");
+        AudioManager.instance.Play("bang");
+        m_munition.Rotate(25,0,0);
+        
     }
     
 }
