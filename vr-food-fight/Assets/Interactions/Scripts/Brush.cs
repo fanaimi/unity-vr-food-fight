e5cb87f14058aaf029aec3f3a1e5e8d31b104d9b
fanaimi <fanaimi@users.noreply.github.com>
2022-01-20 17:15:13 +0000

    completing challenge for session 13

--- vr-food-fight/Assets/Interactions/Scripts/Brush.cs
+++ vr-food-fight/Assets/Interactions/Scripts/Brush.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,11 +10,13 @@ public class Brush : GrabbableObject
     [SerializeField] private Transform m_paintbrushTip;
 
     private GameObject m_spawnedPaint;
+    private TrailRenderer m_trailRend;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+       
     }
 
     
@@ -25,11 +28,10 @@ public class Brush : GrabbableObject
     public override void OnInteractionStart()
     {
         base.OnInteractionStart();
-        print("brush here");
+        //print("brush here");
 
         m_spawnedPaint = Instantiate(m_paintTrailPrefab, m_paintbrushTip.position, m_paintbrushTip.rotation);
-
-        
+        m_trailRend = m_paintTrailPrefab.GetComponent<TrailRenderer>();
     } // oninteractionstart
 
 
@@ -50,4 +52,25 @@ public class Brush : GrabbableObject
 
         m_spawnedPaint.transform.position = m_spawnedPaint.transform.position;
     } // stop
+    
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (m_trailRend != null && other.CompareTag("RedPaint"))
+        {
+            print("red");
+            // m_trailRend.colorGradient.mode = GradientMode.Fixed;
+            m_trailRend.GetComponent<TrailRenderer>().startColor = Color.red;
+            m_trailRend.GetComponent<TrailRenderer>().endColor = Color.white;
+        }
+        
+        
+        if (m_trailRend != null && other.CompareTag("GreenPaint"))
+        {
+            print("green");
+            // m_trailRend.colorGradient.mode = GradientMode.Fixed;
+            m_trailRend.GetComponent<TrailRenderer>().startColor = Color.green;
+            m_trailRend.GetComponent<TrailRenderer>().endColor = Color.white;
+        }
+    }
 }
