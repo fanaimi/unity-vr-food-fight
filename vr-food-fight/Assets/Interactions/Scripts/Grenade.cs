5c21bab3adecce811be2b254c44af585468a2e00
fanaimi <fanaimi@users.noreply.github.com>
2022-01-20 11:02:04 +0000

    setting destructible layer

--- vr-food-fight/Assets/Interactions/Scripts/Grenade.cs
+++ vr-food-fight/Assets/Interactions/Scripts/Grenade.cs
@@ -9,6 +9,12 @@ public class Grenade : FoodItem
     private float m_delay = 3f;
     private float m_countdown;
     private bool m_isActive = false;
+    private bool m_hasExploded = false;
+
+    [SerializeField] private GameObject m_explosionParticles;
+    [SerializeField] private float m_eplosionRadius = 10f;
+    [SerializeField] private float m_explosionForce = 1000f;
+    
     
     void Start()
     {
@@ -23,12 +29,16 @@ public class Grenade : FoodItem
         if (m_isActive)
         {
             m_countdown -= Time.deltaTime;
-            print(m_countdown);
+            if (m_countdown > 0)
+            {
+                print(m_countdown);
+            }
         }
 
-        if (m_countdown <= 0f)
+        if (m_countdown <= 0f && !m_hasExploded)
         {
             Explode();
+            m_hasExploded = true;
         }
 
     } // update
@@ -37,12 +47,48 @@ public class Grenade : FoodItem
     private void Explode()
     {
         AudioManager.instance.Play("Grenade");
-        Debug.Log("Boom");
+        // Debug.Log("Boom");
+        
+        // explosion effect ? particles ? bigExplosionEffect
+        //Instantiate(m_explosionParticles, transform.position, transform.rotation);
+        
+        // getting close objects
+        Collider[] m_closeObjects = Physics.OverlapSphere(transform.position, m_eplosionRadius);
+
+        foreach (Collider m_obj in m_closeObjects)
+        {
+            // adding exploding force
+            Rigidbody m_rb = m_obj.GetComponent<Rigidbody>();
+
+            if (m_rb != null)
+            {
+                if (m_obj.CompareTag("Breakable"))
+                {
+                    m_rb.isKinematic = false;
+                    m_rb.useGravity = true; 
+                }
+
+                m_rb.AddExplosionForce(m_explosionForce, transform.position, m_eplosionRadius);
+            }
+
+            // eventual damage?
+
+
+        }
+         
+            
+       // destroying grenade object
+       Destroy(gameObject);
+        
+        
+        
+        
     } // explode
 
 
     public override void OnInteractionStart()
     {
+        m_isActive = true;
         base.OnInteractionStart();
         print("grenade here");
 
